3f03c09f7e6f1872a16ac4875d8f0da36503dbe5
Moises Santos <Moises Santos@DESKTOP-OCUQ1EB>
2020-03-01 17:32:55 -0300

    treinamento 2

--- src/teste1/Teste1.java
+++ src/teste1/Teste1.java
@@ -15,6 +15,7 @@ public class Teste1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("seila");
         // TODO code application logic here
     }
     
